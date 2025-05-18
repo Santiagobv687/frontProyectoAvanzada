841b55102d70e78c8aeba0cd59fab2eaf8efcd73
Bou <mariac.aranzazur@uqvirtual.edu.co>
2025-05-18 17:38:22 -0500

    se modifica las vistas de inicio y registro

--- src/app/Componentes/inicio/inicio.component.ts
+++ src/app/Componentes/inicio/inicio.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-inicio',
-  imports: [],
   templateUrl: './inicio.component.html',
-  styleUrl: './inicio.component.css'
+  styleUrls: ['./inicio.component.css']
 })
-export class InicioComponent {
+export class InicioComponent implements OnInit {
+  private map!: mapboxgl.Map;
 
+  ngOnInit(): void {
+    this.map = new mapboxgl.Map({
+      accessToken: 'pk.eyJ1IjoiY2FtaWxhMjFkdW4iLCJhIjoiY21hdTgzbmcyMHphYTJtcThmazU1cHo1NiJ9.aBnaFCGSNhwcpNOlRXpl7Q', // Token aquí directamente
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-74.5, 40], // Coordenadas de ejemplo
+      zoom: 9
+    });
+
+    this.map.addControl(new mapboxgl.NavigationControl());
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,RouterModule],
+  standalone: true,
+  imports: [RouterOutlet, RouterModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'My-App';
-  footer = 'Universidad del Quindío'
+  title = '';
+  footer = 'Universidad del Quindío';
 }
